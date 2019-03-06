7c86ecc6fd5179169087e7297e6f0805dff2a8c7
Muhamad Aslan <muhamad.aslan21@gmail.com>
2019-03-06 15:35:30 +0700

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Muhamad Aslan on 06/03/19.
+//  Copyright © 2019 Muh Aslan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
